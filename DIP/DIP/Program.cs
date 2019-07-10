4075bc9813403cb8cf3d095a8ac050a2286a32d1
samurrai <samurrrai.r@gmail.com>
2019-07-11 00:25:41 +0600

    initial

---
+++ DIP/DIP/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    interface IWriter
+    {
+        void Write(string text);
+    }
+
+    class Book {
+
+        public string Text { get; }
+
+        IWriter _writer;
+
+        public Book(IWriter writer, string text)
+        {
+            _writer = writer;
+            Text = text;
+        }
+        public void Write()
+        {
+            _writer.Write(Text);
+        }
+    }
+
+    class FileWriter : IWriter
+    {
+        public void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    class ConsoleWriter : IWriter
+    {
+        public void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWriter writer = new ConsoleWriter();
+            Book book = new Book(writer, "asdsadasda");
+
+            book.Write();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ISP/ISP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    interface ISender {
+        void Send(string message);
+    }
+
+    class MessageSender : ISender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class EmailSender : ISender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISender[] senders = new ISender[2];
+            senders[0] = new MessageSender();
+            senders[1] = new EmailSender();
+            for (int i = 0; i < senders.Length; i++)
+            {
+                senders[i].Send("Hello");
+            }
+            Console.ReadLine();
+        }
+    }
+}
